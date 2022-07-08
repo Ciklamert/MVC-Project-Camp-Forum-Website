4a6674033091f47813b3bcb9ebfa5786329413ec
cikla <cikla@DESKTOP-K7VTIII>
2022-07-08 14:36:52 +0300

    Tema is added.

---
+++ MVCProjeKamp/Controllers/AdminCategoryController.cs
@@ -0,0 +1,43 @@
+﻿using Business.Concrete;
+using Business.ValidationRules;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProjeKamp.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        public IActionResult Index()
+        {
+            var categoryValues = categoryManager.GetList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(category);
+            if (results.IsValid)
+            {
+                categoryManager.CategoryAdd(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
